8535d2a0adb8e71560c53f36f87273d6d3332b68
SKURTZ <areyouskurtz@gmail.com>
2023-08-18 12:06:39 +0300

    NEXT.JS MOGODB CONNECTION

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="flex mx-auto items-center justify-center">
+      Connection to MongoDB
+    </div>
+  );
+}
+
+export default Home;
